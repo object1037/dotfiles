e92fdc683831276b3c149f6ebb2441f5c33091ea
object1037 <contact@object1037.dev>
2022-09-23 14:35:56 +0900

    set up scripts

--- .chezmoiscripts/run_after_build-bat-cache.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eu
-
-bat cache --build
--- .chezmoiscripts/run_once_before_install-brew.sh
+++ .chezmoiscripts/run_once_before_install-brew.sh
@@ -2,9 +2,16 @@
 
 set -eu
 
-if [ ! "$(command -v brew)" ]; then
+if [ "$(command -v brew)" ]; then
   echo "Homebrew already installed."
 else
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+  {{ if eq .chezmoi.arch "arm64" -}}
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bash_profile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  {{ else -}}
+  echo 'eval "$(/usr/local/bin/brew shellenv)"' >> ~/.bash_profile
+  eval "$(/usr/local/bin/brew shellenv)"
 fi
