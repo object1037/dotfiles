4d5190d5875830ce2e309c61fafdfae734763105
object1037 <contact@object1037.dev>
2022-09-24 16:51:03 +0900

    add mac App Store install script

--- .chezmoiscripts/run_onchange_install-brew-packages.sh
+++ .chezmoiscripts/run_onchange_install-brew-packages.sh
@@ -23,6 +23,7 @@ brew "topgrade"
 brew "yarn"
 brew "pnpm"
 brew "neovim"
+brew "mas"
 cask "font-cica"
 cask "font-jetbrains-mono-nerd-font"
 cask "google-chrome"
@@ -32,5 +33,8 @@ cask "hyper"
 cask "raycast"
 cask "fig"
 cask "monitorcontrol"
+mas "GoodNotes", id: 1444383602
+mas "LINE", id: 539883307
+mas "Slack", id: 803453959
 
 EOF
